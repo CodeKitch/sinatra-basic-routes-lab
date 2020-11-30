f1e2c74efdee44a9deb9996ec3260236cd7f2e19
JaSON <masteraccounthub@gmail.com>
2020-11-30 04:14:11 -0500

    commit tree d9e834e4e6dd3519b8134da5da3ca26be25bef8c

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is J.Rete"
+    end
+
+    get '/hometown' do 
+        "My hometown is Eurf"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Move"
+    end
 end
